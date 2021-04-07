5028a3f315c6924e3550ead1c9b6aec9d1635b38
Balaji <balaji.patnam@object-frontier.com>
2021-04-08 01:26:45 +0530

    Init

--- src/index.js
+++ src/index.js
@@ -1,27 +1,33 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
+import React, { Component } from "react";
+import { render } from "react-dom";
 
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
 
-import { createStore, applyMiddleware, compose } from 'redux';
-import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
 
-import rootReducer from './store/reducers';
-import thunk from 'redux-thunk';
+import rootReducer from "./store/reducers";
+import thunk from "redux-thunk";
 
-import App from './app';
-
+import App from "./app";
+console.log("ds");
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(rootReducer, composeEnhancers(
-  applyMiddleware(thunk)
-));
-
-const app = (
-    <Provider store={store}>
-        <Router>        
-            <App />
-        </Router>
-    </Provider>
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
 );
 
-render(app, document.getElementById('root'));
+const app = (
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
+);
+
+render(app, document.getElementById("root"));
