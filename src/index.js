48c12085b5a8f91c4e6fd2ecc63b920ed0256740
Balaji <balaji.patnam@object-frontier.com>
2021-04-10 12:03:41 +0530

    Separated store

--- src/index.js
+++ src/index.js
@@ -3,19 +3,9 @@ import { render } from "react-dom";
 
 import { BrowserRouter as Router } from "react-router-dom";
 
-import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
-
-import rootReducer from "./store/reducers";
-import thunk from "redux-thunk";
-
+import store from "./store";
 import App from "./app";
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
-);
 
 const app = (
   <Provider store={store}>
